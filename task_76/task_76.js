08fc6a7c5e8b3e5f43e93c0cea17334a8e3fcefb
DenisEsipko <iesypko@gmail.com>
2024-03-25 21:33:16 +0200

    add: 76 task fix

--- task_76/task_76.js
+++ task_76/task_76.js
@@ -4,9 +4,10 @@
 // Дан массив. Удалите из него все элементы, которые встречаются только один раз.
 
 
-let arr =[1,2,3,4,5,6,7, 1,2,3,7]; //1237 1237
+let arr = [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 7]; //1237 1237
 
-const removeNonRepeatingElements = (arr) => {
+// вар 1
+const removeNonRepeatingElements1 = (arr) => {
     const count = {};
     arr.forEach((item) => {
         count[item] = (count[item] || 0) + 1;
@@ -16,4 +17,31 @@ const removeNonRepeatingElements = (arr) => {
     });
     return result;
 };
-console.log(removeNonRepeatingElements(arr));
+console.log(removeNonRepeatingElements1(arr," var 1"));
+
+// вариант 2
+const removeNonRepeatingElements2 = (arr) => {
+    let temp = [];
+    arr.forEach(item => {
+        let last = arr.lastIndexOf(item);
+        let first = arr.indexOf(item);
+        if (last !== first) {
+            temp.push(arr[first])
+        }
+    })
+    return temp;
+};
+console.log(removeNonRepeatingElements2(arr," var 2"));
+
+// вар 3
+function removeNonRepeatingElements3(arr) {
+    let uniqueArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniqueArr.includes(arr[i])) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+    return uniqueArr;
+}
+console.log(removeNonRepeatingElements3(arr)," var 3");
